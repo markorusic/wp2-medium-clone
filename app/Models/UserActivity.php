26d0d41b325801814747f715f0a3d70968f52487
Marko Rusic <markousp5@gmail.com>
2020-01-05 16:34:25 +0100

    added fillable props to Like and UserActivity models

--- app/Models/UserActivity.php
+++ app/Models/UserActivity.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class UserActivity extends Model
 {
+    protected $fillable = ['activity'];
+
     public function user() {
         return $this->belongsTo('App\Models\User');
     }
