c06fd25df88b204c99362730943d453d69f1ebc0
Marko Rusic <markousp5@gmail.com>
2020-01-16 01:06:51 +0100

    user profile layout update

--- resources/views/public/pages/user-profile.blade.php
+++ resources/views/public/pages/user-profile.blade.php
@@ -3,6 +3,12 @@
 @section('content')
     <div class="my-3 px-5 pt-5">
         <div class="d-flex justify-content-between my-4">
+            <div class="mr-5">
+                @include('public.user.user-avatar', [
+                    'size' => 'lg',
+                    'user' => $user
+                ])
+            </div>
             <div class="d-flex flex-column">
                 <div class="d-flex align-items-center">
                     <h1 class="mr-3">{{ $user->name }}</h1>
@@ -19,19 +25,30 @@
                     <h5 class="text-dark font-weight-normal">{{ $user->bio }}</h5>
                 </div>
                 <div class="my-2">
-                    <span class="text-secondary mr-3">
-                        {{ $user->following_count }} Following
+                    <span class="text-secondary fs-18 mr-3">
+                        <span class="font-weight-600 text-black">
+                            {{ $posts->count() }}
+                        </span> posts
                     </span>
-                    <span class="text-secondary">
-                        <span data-followers-count>{{ $user->followers_count }}</span> Followers
-                    </span>
+                    <a href="#"
+                        class="text-secondary fs-18 mr-3 pointer"
+                        data-toggle="modal"
+                        data-target="#followers-modal"
+                     >
+                        <span class="font-weight-600 text-black" data-followers-count>
+                            {{ $user->followers_count }}
+                        </span> followers
+                    </a>
+                    <a  href="#"
+                        class="text-secondary fs-18 mr-3 pointer"
+                        data-toggle="modal"
+                        data-target="#following-modal"
+                    >
+                        <span class="font-weight-600 text-black">
+                            {{ $user->following_count }}
+                        </span> following
+                    </a>
                 </div>
-            </div>
-            <div class="ml-5">
-                @include('public.user.user-avatar', [
-                    'size' => 'lg',
-                    'user' => $user
-                ])
             </div>
         </div>
 
