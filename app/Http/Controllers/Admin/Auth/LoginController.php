ab4a005ce6be0d73bef140c6cb679901d862a1cc
Marko Rusic <markousp5@gmail.com>
2020-01-18 03:11:08 +0100

    admin login ctrl cleanup

--- app/Http/Controllers/Admin/Auth/LoginController.php
+++ app/Http/Controllers/Admin/Auth/LoginController.php
@@ -15,42 +15,22 @@ class LoginController extends Controller
         $this->middleware('guest:admin')->except('logout');
     }
 
-    /**
-     * Show the login form.
-     * 
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
         return view('admin.auth.login');
     }
 
-    /**
-     * Login the admin.
-     * 
-     * @param \Illuminate\Http\Request $request
-     * @return \Illuminate\Http\RedirectResponse
-     */
     public function login(AdminLoginRequest $request)
     {   
         $credentials = $request->only('email','password');
         $remember = $request->filled('remember');
         if(auth('admin')->attempt($credentials, $remember)){
             return redirect()
-                ->intended(route('admin.home'))
-                ->with('status','You are Logged in as Admin!');
+                ->intended(route('admin.home'));
         }
-        return redirect()
-            ->back()
-            ->withInput()
-            ->with('error','Login failed, please try again!');
+        return redirect()->back();
     }
 
-    /**
-     * Logout the admin.
-     * 
-     * @return \Illuminate\Http\RedirectResponse
-     */
     public function logout()
     {
         auth('admin')->logout();
