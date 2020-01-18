1f7215166a6bf4e5d20162437d4505071158b01b
Marko Rusic <markousp5@gmail.com>
2020-01-18 03:37:47 +0100

    auth login ctrl improved responses

--- app/Http/Controllers/Admin/Auth/LoginController.php
+++ app/Http/Controllers/Admin/Auth/LoginController.php
@@ -2,8 +2,6 @@
 
 namespace App\Http\Controllers\Admin\Auth;
 
-use App\Http\Requests\AdminLoginRequest;
-
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\ThrottlesLogins;
@@ -20,14 +18,24 @@ class LoginController extends Controller
         return view('admin.auth.login');
     }
 
-    public function login(AdminLoginRequest $request)
+    public function login(Request $request)
     {   
+        $this->validate($request, [
+            'email'    => 'required|email|exists:admins|min:5|max:191',
+            'password' => 'required|string|min:4|max:255'
+        ]);
+
         $credentials = $request->only('email','password');
         $remember = $request->filled('remember');
-        if(auth('admin')->attempt($credentials, $remember)){
+
+        if(!auth('admin')->attempt($credentials, $remember)){
             return redirect()->intended(route('admin.home'));
         }
-        return redirect()->back();
+
+        return redirect()
+            ->back()
+            ->withInput($request->only('email', 'remember'))
+            ->withErrors([ 'email' => 'Auth failed' ]);
     }
 
     public function logout()
